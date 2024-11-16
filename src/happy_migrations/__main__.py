5a36e514d656b9aaccc921e158cd5b1fac6b4ced
Simon <szymonpiechutowski@gmail.com>
2024-11-16 16:37:45 +0100

    Created StatusApp and added to CLI

--- src/happy_migrations/__main__.py
+++ src/happy_migrations/__main__.py
@@ -6,6 +6,7 @@ from happy_migrations import (
     parse_happy_ini,
     create_happy_ini
 )
+from happy_migrations._textual_app import StatusApp
 
 
 class SQLiteBackendContext(click.Context):
@@ -15,7 +16,7 @@ class SQLiteBackendContext(click.Context):
 @click.group()
 @click.pass_context
 def happy(ctx) -> None:
-    """A CLI tool for managing SQL migrations."""
+    """Happy CLI."""
     happy_ini = parse_happy_ini()
     ctx.obj = SQLiteBackend(happy_ini)
 
@@ -23,7 +24,7 @@ def happy(ctx) -> None:
 @click.command()
 @click.pass_context
 def init(ctx: SQLiteBackendContext) -> None:
-    """Initializes the Happy migration system """
+    """Initializes the Happy migration system."""
     ctx.obj.happy_init()
     ctx.obj.close_connection()
 
@@ -45,6 +46,47 @@ def config():
         echo(style("Warning: ", "yellow") + message)
 
 
+@click.command()
+@click.pass_context
+def log(ctx: SQLiteBackendContext) -> None:
+    """Display _happy_log table."""
+
+
+@click.command()
+@click.pass_context
+def status(ctx: SQLiteBackendContext) -> None:
+    """Display _happy_status table."""
+    status_data = ctx.obj.list_happy_status()
+    StatusApp(
+        headers=["Name", "Status", "Creation Date"],
+        rows=status_data
+    ).run(inline=True, inline_no_clear=True)
+
+
+@click.command()
+@click.pass_context
+def fixture(ctx: SQLiteBackendContext):
+    """Create 1000 migrations with names based on 孫子 quotes names."""
+    from random import randint
+    quotes = [
+        "all_warfare_is_based_on_deception",
+        "the_wise_warrior_avoids_the_battle",
+        "in_the_midst_of_chaos_opportunity",
+        "move_swift_as_the_wind",
+        "strategy_without_tactics_is_slow",
+        "let_your_plans_be_dark",
+        "supreme_art_is_to_subdue",
+        "opportunities_multiply_as_they_are_seized",
+        "he_will_win_who_knows_when_to_fight",
+        "quickness_is_the_essence_of_war"
+    ]
+    for _ in range(10**3):
+        ctx.obj.create_mig(quotes[randint(0, 9)])
+
+
 happy.add_command(init)
 happy.add_command(cmig)
 happy.add_command(config)
+happy.add_command(log)
+happy.add_command(status)
+happy.add_command(fixture)
