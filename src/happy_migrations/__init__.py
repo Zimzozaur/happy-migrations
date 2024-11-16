9072c39339c84e978fdd68494fdfea46d189d9ad
Simon <szymonpiechutowski@gmail.com>
2024-11-16 14:04:09 +0100

    Added SQLiteBackend and parse_happy_ini  to __init__.py

--- src/happy_migrations/__init__.py
+++ src/happy_migrations/__init__.py
@@ -1,8 +1,12 @@
 """happy_migrations' __init__.py"""
 
 from ._data_classes import Step, Migration
+from .sqlite_backend import SQLiteBackend
+from .parse_ini import parse_happy_ini
 
 __all__ = [
     "Migration",
     "Step",
+    "SQLiteBackend",
+    "parse_happy_ini",
 ]
